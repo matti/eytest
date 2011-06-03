569124a6ee3d2809fea16e3be1337775bde65701
Matti Paksula <matti.paksula@iki.fi>
2011-06-03 20:03:40 +0300

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Eytest::Application.config.secret_token = '7bc0efd0a9d0526c630604cdf51572ab43f078dac78d0929d4b27a182ec5fff7bf543603d749f9a73432c6dec2ee4cec857f850a35df53a100300818c9085dcf'
---
+++ db/migrate/20110603170134_create_cats.rb
@@ -0,0 +1,13 @@
+class CreateCats < ActiveRecord::Migration
+  def self.up
+    create_table :cats do |t|
+      t.string :name
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :cats
+  end
+end
